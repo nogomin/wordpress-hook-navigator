4291948f553cabe2d52fd4d8950f2f07ce291b15
nogomin <nogomindev@gmail.com>
2021-04-03 22:21:20 +0900

    feat : find function & tag definition

---
+++ extension.js
@@ -0,0 +1,130 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+function activate(context) {
+
+	let disposable = vscode.commands.registerCommand('extension.goToHookDefinition', async function (fileUri) {
+
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor.selection;
+		const text = editor.document.getText(selection);
+		const currentLine = editor.document.lineAt(editor.selection.active.line)._text;
+
+		let type = '';
+		const actionNames = ['add_action', 'do_action', 'add_filter', 'apply_filters'];
+		const regex = /('|")(.*?)('|")/ig;
+		const check = currentLine.match(regex);
+		
+		if(check.length === 1) {
+			actionNames.forEach(action => {
+				if(currentLine.includes(action)) {
+					type = action;
+				}
+			})
+		} else if(check.length > 1) {
+			if(check[1] === `'${text}'`) {
+				type = 'function';
+			} else {
+				const regex = /(\w)+/ig;
+				const check = currentLine.match(regex);
+				const actionName = check[0];
+				type = actionName;
+			}
+		}
+
+		if(type === 'function') {
+			const location = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', fileUri);
+	
+			if(location) {
+				const foundFunction = [];
+				location.forEach(item => {
+					if(item.children.length > 0) {
+						item.children.forEach(val => {
+							foundFunction.push(val.name.includes(text));
+						})
+					} else {
+						foundFunction.push(item.name.includes(text));
+					}
+				})
+				const existFunctionInThisFile = foundFunction.includes(true);
+	
+				if(existFunctionInThisFile) {
+					location.forEach((item) => {
+						if(item.name === text) {
+							const lineNumber = item.range._start._line;
+							vscode.commands.executeCommand('editor.action.goToLocations', vscode.window.activeTextEditor.document.uri, new vscode.Position(lineNumber, 0), []);
+						} else {
+							item.children.forEach((val) => {
+								if(val.name === text) {
+									const lineNumber = val.range._start._line;
+									vscode.commands.executeCommand('editor.action.goToLocations', vscode.window.activeTextEditor.document.uri, new vscode.Position(lineNumber, 0), []);
+								}
+							})
+						}
+					})
+				} else {
+					vscode.commands.executeCommand('workbench.action.findInFiles', {
+						query: 'function\\s+' + text,
+						triggerSearch: true,
+						isRegex: true,
+						isCaseSensitive: false,
+						matchWholeWord: false
+					});
+				}
+			} else {
+				vscode.commands.executeCommand('workbench.action.findInFiles', {
+					query: 'function\\s+' + text,
+					triggerSearch: true,
+					isRegex: true,
+					isCaseSensitive: false,
+					matchWholeWord: false
+				});
+			}
+		} else {
+			if(type === 'add_action') {
+				vscode.commands.executeCommand('workbench.action.findInFiles', {
+					query: 'do_action(\\s+)?\\((\\s+)?\'' + text,
+					triggerSearch: true,
+					isRegex: true,
+					isCaseSensitive: false,
+					matchWholeWord: false
+				});
+			} else if(type === 'do_action') {
+				vscode.commands.executeCommand('workbench.action.findInFiles', {
+					query: 'add_action(\\s+)?\\((\\s+)?\'' + text,
+					triggerSearch: true,
+					isRegex: true,
+					isCaseSensitive: false,
+					matchWholeWord: false
+				});
+			} else if(type === 'add_filter') {
+				vscode.commands.executeCommand('workbench.action.findInFiles', {
+					query: 'apply_filters(\\s+)?\\((\\s+)?\'' + text,
+					triggerSearch: true,
+					isRegex: true,
+					isCaseSensitive: false,
+					matchWholeWord: false
+				});
+			} else if(type === 'apply_filters') {
+				vscode.commands.executeCommand('workbench.action.findInFiles', {
+					query: 'add_filter(\\s+)?\\((\\s+)?\'' + text,
+					triggerSearch: true,
+					isRegex: true,
+					isCaseSensitive: false,
+					matchWholeWord: false
+				});
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
